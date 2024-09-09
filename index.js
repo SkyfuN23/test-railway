41f58fdedeb640faf665e41ef78a9096f0d4673e
SkyfuN23 <juli10capo@hotmail.com>
2024-09-09 15:36:30 -0300

    cambio

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     try {
+        console.log('Iniciando Puppeteer...');
         const browser = await puppeteer.launch({
             headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox'] // Necesario para entornos como Railway
         });
 
+        console.log('Navegador iniciado, abriendo nueva página...');
         const page = await browser.newPage();
+        
+        console.log('Cargando página de Google...');
         await page.goto('https://www.google.com');
+        console.log('Página cargada, tomando captura de pantalla...');
 
         // Guardar la captura de pantalla
         const filePath = path.join(__dirname, 'google.png');
         await page.screenshot({ path: filePath });
-
         console.log('Captura de pantalla tomada: google.png');
 
         await browser.close();
+        console.log('Navegador cerrado.');
 
         // Enviar la captura de pantalla como respuesta
         res.sendFile(filePath);
